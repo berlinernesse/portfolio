aaef42f247d609c960c61aa91c39839351f111c3
berlinernesse <berlin.em@proton.me>
2025-06-16 15:42:08 -0400

    worked on the background and toggle theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react";
+import { useFetcher } from "react-router-dom";
+import { cn } from "../lib/utils";
 
 export const ThemeToggle =()=> {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
+    useEffect(()=> {
+        const storedTheme = localStorage.getItem("theme");
 
-    return <button>
-        {""}
-        {isDarkMode ? 
-        (<Sun className="h-6 w-g text-yellow-300"/> 
-        ) : (
-        <Moon className="h-6 w-g text-blue-900"/>
+        if(storedTheme === "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.add("dark");
+        } else {
+            setIsDarkMode(false)
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false)
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true)
+        }
+    };
+
+    return(
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300","focus:outnlin-hidden"
         )}
-    </button>
+        >
+            {""}
+            {isDarkMode ? (<Sun className="h-6 w-g text-yellow-300"/> 
+            ) : (
+            <Moon className="h-6 w-g text-blue-900"/>)}
+        </button>
+    ) 
 }
