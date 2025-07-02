48914a47826b98583139b5a277611bfd69ababdd
berlinernesse <berlin.em@proton.me>
2025-07-02 15:35:48 -0400

    worked on navbar and hero section, currently stuck on about section

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,46 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection =()=>{
+    return (
+        <section id="about" className="py-24 px-4 relatives">
+            {" "}
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    About <span className="text-primary">Me</span>
+                </h2>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                    <div className="space-y-6">
+                        <h3 className="text-2xl font-semibold">
+                            Passionate Something Something
+                        </h3>
+
+                        <p className="text-muted-foreground">
+                            With a lot of years and more years. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia vitae reprehenderit incidunt sunt quaerat nisi nemo qui, enim molestias quibusdam praesentium expedita corporis delectus nulla nostrum aut pariatur magni voluptatem!
+                        </p>
+
+                        <p className="text-muted-foreground">
+                            With a lot of years and more years. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia vitae reprehenderit incidunt sunt quaerat nisi nemo qui, enim molestias quibusdam praesentium expedita corporis delectus nulla nostrum aut pariatur magni voluptatem!
+                        </p>
+
+                        <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+                        
+
+                            <a
+                                href="#contact"
+                                className="cosmic-button">
+                            
+                                 Get in Touch
+                            </a>
+                                            
+                          
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+    
+}
+    
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground"
 import { NavBar } from "../components/NavBar"
+import { HeroSection } from "../components/HeroSection"
+import { AboutSection } from "../components/AboutSection"
 
 export const Home =()=> {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -13,7 +15,18 @@ export const Home =()=> {
         {/*navbar*/}
         <NavBar />
 
+
         {/*main content*/}
+
+        <main>
+            <HeroSection />
+            <AboutSection />
+            {/*
+                
+                <ProjectsSection />
+                <ContactSection />
+            */}
+        </main>
 
         {/*footer*/}
     </div>
